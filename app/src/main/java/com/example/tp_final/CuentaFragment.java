b570fe908dfad63c14fa9df5efc297cf501dfe31
MatiasAlvesVentura <matias.alves@alumnos.frgp.utn.edu.ar>
2022-10-17 19:34:59 -0300

    Navigation Drawer

---
+++ app/src/main/java/com/example/tp_final/CuentaFragment.java
@@ -0,0 +1,28 @@
+package com.example.tp_final;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CuentaFragment extends Fragment {
+
+    public CuentaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cuenta, container, false);
+    }
+}
