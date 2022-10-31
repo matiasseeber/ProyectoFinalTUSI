1bb0f03d63f41ea0b497d57ea714bb7fff4d3d30
MatiasAlvesVentura <matias.alves@alumnos.frgp.utn.edu.ar>
2022-10-31 19:20:40 -0300

    Arreglos

--- app/src/main/java/com/example/tp_final/Metodos_Pago.java
+++ app/src/main/java/com/example/tp_final/Metodos_Pago.java
@@ -3,6 +3,7 @@ package com.example.tp_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Metodos_Pago extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Metodos_Pago extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodos_pago);
+    }
+
+    public void ClickBack(View view){
+        this.finish();
     }
 }
